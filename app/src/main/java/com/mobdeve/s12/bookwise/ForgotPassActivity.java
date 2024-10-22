d75b22466129773975cd7dbe5c2965f73ea9db54
Shawn Chen <wtan984@gmail.com>
2024-10-22 15:17:29 +0800

    added Forgot Password

--- app/src/main/java/com/mobdeve/s12/bookwise/ForgotPassActivity.java
+++ app/src/main/java/com/mobdeve/s12/bookwise/ForgotPassActivity.java
@@ -1,6 +1,12 @@
 package com.mobdeve.s12.bookwise;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ForgotPassActivity extends AppCompatActivity {
 
+    private Button btnConfirm;
+    private EditText emailInput, passWord, confirmPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_forgot_pass);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        initViews();
+        btnConfirm.setOnClickListener(v -> changePassword());
+    }
+
+    public void initViews(){
+        emailInput = findViewById(R.id.fp_email);
+        passWord = findViewById(R.id.fp_password);
+        confirmPass = findViewById(R.id.fp_confirmPass);
+        btnConfirm = findViewById(R.id.fp_confirm);
+    }
+
+    public void changePassword(){
+        String email = emailInput.getText().toString();
+        String newPassword = passWord.getText().toString();
+        String confirmPassword = confirmPass.getText().toString();
+
+        if (email.isEmpty() || newPassword.isEmpty() || confirmPassword.isEmpty()) {
+            Toast.makeText(ForgotPassActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+        } else if (!newPassword.equals(confirmPassword)) {
+            Toast.makeText(ForgotPassActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(ForgotPassActivity.this, "Password reset successful", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(ForgotPassActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
